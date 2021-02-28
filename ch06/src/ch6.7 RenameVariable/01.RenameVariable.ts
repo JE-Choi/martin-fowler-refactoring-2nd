7602e33f3ff612925f719e3597585aff8ab89da7
최지은 <lielieing@naver.com>
2021-02-28 22:19:58 +0900

    예시) [후] 변수이름 바꾸기-2 - 변수명 변경 - Data 클래스 생성해서 데이터 캡슐화시키고, 데이터 private으로 변경

--- ch06/src/ch6.7 RenameVariable/01.RenameVariable.ts
+++ ch06/src/ch6.7 RenameVariable/01.RenameVariable.ts
@@ -1,22 +1,25 @@
 {
-    let tpHd = "untitled";
+    class Data {
+        private title = "untitled";
 
-    // 변수 캡슐화 → 데이터에 접근하는 set, getter만들기
-    function title() {
-        return tpHd
-    };
+        // 변수 캡슐화 → 데이터에 접근하는 set, getter만들기
+        getTitle() {
+            return this.title
+        };
 
-    function setTitle(arg) {
-        tpHd = arg
-    };
+        setTitle(arg) {
+            this.title = arg
+        };
+    }
 
     let result = "";
     const obj = {
         'articleTitle': 'articleTitle'
     }
-    result += `<h1>${title()}</h1>`; // 값 참조도 됨.
-    setTitle(obj['articleTitle']); // 값 수정도 되고
-    console.log(tpHd);
+    const data = new Data();
+    result += `<h1>${data.getTitle()}</h1>`; // 값 참조도 됨.
+    data.setTitle(obj['articleTitle']); // 값 수정도 되고
+    console.log(data.getTitle());
     console.log(result);
 
 }
