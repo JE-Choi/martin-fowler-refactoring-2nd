8b195d0fe9499a149973971f11d9b0994d252e05
최지은 <lielieing@naver.com>
2021-02-28 22:17:23 +0900

    예시) [후] 변수이름 바꾸기-1 - 변수 캡슐화 → 데이터에 접근하는 set,getter만들기 - 데이터는 set,getter로 접근하기

--- ch06/src/ch6.7 RenameVariable/01.RenameVariable.ts
+++ ch06/src/ch6.7 RenameVariable/01.RenameVariable.ts
@@ -1,9 +1,22 @@
-let tpHd = "untitled";
-let result = "";
-const obj = {
-    'articleTitle': 'articleTitle'
+{
+    let tpHd = "untitled";
+
+    // 변수 캡슐화 → 데이터에 접근하는 set, getter만들기
+    function title() {
+        return tpHd
+    };
+
+    function setTitle(arg) {
+        tpHd = arg
+    };
+
+    let result = "";
+    const obj = {
+        'articleTitle': 'articleTitle'
+    }
+    result += `<h1>${title()}</h1>`; // 값 참조도 됨.
+    setTitle(obj['articleTitle']); // 값 수정도 되고
+    console.log(tpHd);
+    console.log(result);
+
 }
-result += `<h1>${tpHd}</h1>`; // 값 참조도 됨.
-tpHd = obj['articleTitle']; // 값 수정도 되고
-console.log(tpHd);
-console.log(result);
