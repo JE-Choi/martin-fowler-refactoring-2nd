d5d97603d8726d11006a2ad71702dd08f043457f
최지은 <lielieing@naver.com>
2021-02-28 19:54:11 +0900

    예시) [후] 함수 이름 바꾸기(마이그레이션 절차)-1 - 함수 본문 전체를 새로운 함수로 - 본래 함수에 @deprecated

--- ch06/src/ch06_5_ChangeFunctionDeclaration/02.ChangeFuntionName(Migration Step).ts
+++ ch06/src/ch06_5_ChangeFunctionDeclaration/02.ChangeFuntionName(Migration Step).ts
@@ -1,6 +1,13 @@
 class ChangeFuntionName_MigrationStep{
+    /**
+     * @deprecated
+     * @param radius
+     */
     circum(radius: number) {
+        return this.circumference(radius);
+    }
+    circumference(radius: number) {
         return 2 * Math.PI * radius;
     }
 }
-console.log(new ChangeFuntionName_MigrationStep().circum(1));
+console.log(new ChangeFuntionName_MigrationStep().circum(2));
