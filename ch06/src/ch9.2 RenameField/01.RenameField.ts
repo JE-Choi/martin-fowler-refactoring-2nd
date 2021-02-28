575bbc19e73d1aa8ea08c3d07912da6c93d6f622
최지은 <lielieing@naver.com>
2021-02-28 22:50:39 +0900

    예시) [전] 필드이름 바꾸기

---
+++ ch06/src/ch9.2 RenameField/01.RenameField.ts
@@ -0,0 +1,11 @@
+{
+    const organization = {name: "애크미 구스베리", cuntry: "GB"};
+
+    function makeString ():string {
+        return `${organization.name}: ${organization.cuntry}`;
+    }
+
+    /**
+     * 요구사항: organization의 name속성을 title로 변경하고 싶음
+     */
+}
