0654a4641b00b2a14b04a7373a8c11b0d02703e0
최지은 <lielieing@naver.com>
2021-02-28 23:03:20 +0900

    예시) [후] 필드이름 바꾸기 - data.name, data.title도 모두 데이터로 담을 수 있지만, data.title이 우선이 되도록

--- ch06/src/ch9.2 RenameField/01.RenameField.ts
+++ ch06/src/ch9.2 RenameField/01.RenameField.ts
@@ -2,22 +2,21 @@
     /**
      * 요구사항: organization의 name속성을 title로 변경하고 싶음
      */
-    // 변수를 클래스로 캡슐화
     class Organization {
-        private _name: string = "";
+        private _title: string = "";
         private _country: string = "";
 
-        constructor(data: { name: string, country: string }) {
-            this._name = data.name;
+        constructor(data: { name?: string, country: string, title?: string }) {
+            this._title = data.title !== undefined ? data.title : data.name;
             this._country = data.country;
         }
 
-        get name() {
-            return this._name;
+        get title() {
+            return this._title;
         }
 
-        set name(aString) {
-            this._name = aString;
+        set title(aString) {
+            this._title = aString;
         }
 
         get country() {
@@ -31,7 +30,7 @@
 
     function makeString(): string {
         const organization: Organization = new Organization({name: "애크미 구스베리", country: "GB"});
-        return `${organization.name}: ${organization.country}`;
+        return `${organization.title}: ${organization.country}`;
     }
 
     console.log(makeString());
