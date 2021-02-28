156ccacb954dfb7de32294398c8462774541ae26
최지은 <lielieing@naver.com>
2021-02-28 19:50:46 +0900

    예시) [전] 함수 이름 바꾸기(마이그레이션 절차)-2(클래스생성)

---
+++ ch06/src/ch06_5_ChangeFunctionDeclaration/01.ChangeFuntionName(Simple Step).ts
@@ -0,0 +1,7 @@
+class ChangeFuntionName_SimpleStep {
+    circum(radius: number) {
+        return 2 * Math.PI * radius;
+    }
+}
+
+console.log(new ChangeFuntionName_SimpleStep().circum(1));
--- ch06/src/ch06_5_ChangeFunctionDeclaration/ChangeFuntionName(Migration Step).ts
+++
@@ -1,5 +0,0 @@
-function circum(radius: number) {
-    return 2 * Math.PI * radius;
-}
-
-console.log(circum(1));
