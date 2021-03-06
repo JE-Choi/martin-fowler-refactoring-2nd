d33e3e32952720b6ee406874873d301ee4b4526e
최지은 <lielieing@naver.com>
2021-03-07 03:22:55 +0900

    지역변수를 사용할때

--- ch06/src/ch6_1 Extract Function/유효범위를 벗어나는 변수가 없을때.ts
+++ ch06/src/ch6_1 Extract Function/유효범위를 벗어나는 변수가 없을때.ts
@@ -1,43 +1,47 @@
-type Invoice = {
-    customer: string,
-    orders: {
-        amount: number
-    }[],
-    dueDate?: Date
-}
-function printOwing(invoice: Invoice) {
-    function printBanner(){
-        console.log("*******************");
-        console.log("******고객채무*****");
-        console.log("*******************");
+{
+    type Invoice = {
+        customer: string,
+        orders: {
+            amount: number
+        }[],
+        dueDate?: Date
     }
 
-    function printDetails(){
-        console.log(`고객명: ${invoice.customer}`);
-        console.log(`채무액: ${outStanding}`);
-        console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    // @ts-ignore
+    function printOwing(invoice: Invoice) {
+        function printBanner() {
+            console.log("*******************");
+            console.log("******고객채무*****");
+            console.log("*******************");
+        }
+
+        function printDetails() {
+            console.log(`고객명: ${invoice.customer}`);
+            console.log(`채무액: ${outStanding}`);
+            console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+        }
+
+        let outStanding = 0;
+
+        printBanner();
+
+        // 미 해결 채무(outStanding)을 계산한다.
+        for (const o of invoice.orders) {
+            outStanding += o.amount;
+        }
+
+        const today: Date = new Date();
+        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+        printDetails();
+
     }
 
-    let outStanding = 0;
-
-    printBanner();
-
-    // 미 해결 채무(outStanding)을 계산한다.
-    for (const o of invoice.orders) {
-        outStanding += o.amount;
+    const invoice: Invoice = {
+        customer: "고객",
+        orders: [{
+            amount: 1000
+        }]
     }
-
-    const today: Date = new Date();
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-    printDetails();
-
+    printOwing(invoice);
 }
-
-const invoice: Invoice = {
-    customer: "고객",
-    orders: [{
-        amount: 1000
-    }]
-}
-printOwing(invoice);
---
+++ ch06/src/ch6_1 Extract Function/지역변수를 사용할때.ts
@@ -0,0 +1,57 @@
+{
+    type Invoice = {
+        customer: string,
+        orders: {
+            amount: number
+        }[],
+        dueDate?: Date
+    }
+
+    // @ts-ignore
+    function printDetails(invoice: Invoice, outStanding: number) {
+        console.log(`고객명: ${invoice.customer}`);
+        console.log(`채무액: ${outStanding}`);
+        console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    }
+
+    /**
+     * 배열, 레코드, 객체와 같은 데이터구조라면(주소값을 참조하는 데이터)
+     * 함수에서 데이터를 수정했을때, 반환하지 않아도 원본 데이터 수정이 가능하다.
+     */
+    // @ts-ignore
+    function recordDueDate() {
+        const today: Date = new Date();
+        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    }
+
+    // @ts-ignore
+    function printOwing(invoice: Invoice) {
+        function printBanner() {
+            console.log("*******************");
+            console.log("******고객채무*****");
+            console.log("*******************");
+        }
+
+        let outStanding = 0;
+
+        printBanner();
+
+        // 미 해결 채무(outStanding)을 계산한다.
+        for (const o of invoice.orders) {
+            outStanding += o.amount;
+        }
+
+        recordDueDate();
+
+        printDetails(invoice, outStanding);
+
+    }
+
+    const invoice: Invoice = {
+        customer: "고객",
+        orders: [{
+            amount: 1000
+        }]
+    }
+    printOwing(invoice);
+}
