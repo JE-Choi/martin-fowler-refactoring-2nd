127de0b4b8718139add18d15e0326ac52e2e9989
최지은 <lielieing@naver.com>
2021-03-07 14:19:21 +0900

    [후] 메서드 올리기3 - 서브 클래스 책임 오류

--- ch12/ch12.1 Pull Up Method/Example.ts
+++ ch12/ch12.1 Pull Up Method/Example.ts
@@ -1,18 +1,18 @@
-class Party{
+class Party {
+    get monthlyCost(): number {
+        throw new Error("서브클래스 책임 오류");
+    }
 
-}
-
-class Employee extends Party{
-    monthlyCost: number = 1;
-
-    get annualCost() {
+    get annualCost():number {
         return this.monthlyCost * 12;
     }
 }
 
-class Department extends Party{
-    monthlyCost: number = 1;
-    get annualCost() {
-        return this.monthlyCost * 12;
-    }
+class Employee extends Party {
 }
+
+class Department extends Party {
+}
+
+const person: Party = new Employee();
+console.log(person.annualCost);
