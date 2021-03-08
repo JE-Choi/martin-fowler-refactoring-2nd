3b13cd13e322fdc3c273950db0d16965f6e30b16
최지은 <lielieing@naver.com>
2021-03-08 21:01:36 +0900

    [후] 임시변수를 질의 함수로 바꾸기

--- ch07/7.4_Replace Temp with Query/src/임시변수를 질의 함수로 바꾸기.ts
+++ ch07/7.4_Replace Temp with Query/src/임시변수를 질의 함수로 바꾸기.ts
@@ -7,12 +7,21 @@ class Order {
     }
 
     get price() {
-        const basePrice = this.quantity * this.item.price;
+        return this.basePrice * this.discountFactor;
+    }
+
+    get basePrice(){
+        return this.quantity * this.item.price;
+    }
+
+    get discountFactor(){
         let discountFactor = 0.98;
 
-        if(basePrice > 1000){
+        if (this.basePrice > 1000) {
             discountFactor -= 0.03;
         }
-        return basePrice * discountFactor;
+        return discountFactor;
     }
 }
+
+console.log(new Order(10, {price: 1000}).price);
