d7f300f19ea0d02d89a7ba481ff3f1e515046d80
최지은 <lielieing@naver.com>
2021-03-21 13:44:55 +0900

    ch10.1 조건문 분해하기 [리펙토링 전]

---
+++ ch10/ch10.1 Decompse Conditional/ch10.1 조건문 분해하기.ts
@@ -0,0 +1,47 @@
+{
+    class SeasonChecker {
+        date = new Date();
+        isBefore = (month: number) => {
+            return this.date.getMonth() < month;
+        }
+
+        isAfter = (month: number) => {
+            return month < this.date.getMonth();
+        }
+    }
+
+    type Plan = {
+        summer: {
+            start: number;
+            end: number;
+            rate: number;
+        },
+        regular: {
+            rate: number;
+            serviceCharge: number;
+        }
+    }
+    const plan: Plan = {
+        summer: {
+            start: 7,
+            end: 10,
+            rate: 0.8
+        },
+        regular: {
+            rate: 1.0,
+            serviceCharge: 1000
+        }
+    }
+
+    const aDate: SeasonChecker = new SeasonChecker();
+    let charge = 0;
+    let quantity = 3;
+    // 여름철이면 할인율 달라짐.
+    if (!aDate.isBefore(plan.summer.start) && !aDate.isAfter(plan.summer.end)) {
+        charge = quantity * plan.summer.rate;
+    } else {
+        charge = quantity * plan.regular.rate + plan.regular.serviceCharge;
+    }
+
+    console.log(charge);
+}
