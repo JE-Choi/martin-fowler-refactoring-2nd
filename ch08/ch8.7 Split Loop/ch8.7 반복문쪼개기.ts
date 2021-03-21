8c08c2db15c030120f35ba48225d939243e64e24
최지은 <lielieing@naver.com>
2021-03-21 16:06:41 +0900

    ch8.7 반복문 쪼개기 [리팩토링1] - 함수추출, 함수인라인 호출

--- ch08/ch8.7 Split Loop/ch8.7 반복문쪼개기.ts
+++ ch08/ch8.7 Split Loop/ch8.7 반복문쪼개기.ts
@@ -3,21 +3,27 @@
         age: number;
         salary: number;
     }
+
+    const totalSalary = () => {
+        let totalSalary = 0;
+        for (const p of people) {
+            totalSalary += p.salary;
+        }
+        return totalSalary;
+    }
+
+    const youngestAge = () => {
+        let youngest = people[0] ? people[0].age : Infinity;
+        for (const p of people) {
+            if (p.age < youngest) youngest = p.age;
+        }
+        return youngest;
+    }
+
     const p1: People = {age: 20, salary: 3000};
     const p2: People = {age: 15, salary: 5000};
     const p3: People = {age: 25, salary: 4000};
     const people: People[] = [p1, p2, p3];
 
-
-    let youngest = people[0] ? people[0].age : Infinity;
-    for (const p of people) {
-        if (p.age < youngest) youngest = p.age;
-    }
-
-    let totalSalary = 0;
-    for (const p of people) {
-        totalSalary += p.salary;
-    }
-
-    console.log(`최연소: ${youngest}, 총 급여: ${totalSalary}`);
+    console.log(`최연소: ${youngestAge()}, 총 급여: ${totalSalary()}`);
 }
