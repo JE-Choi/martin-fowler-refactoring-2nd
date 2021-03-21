e2fa39245a77fe7e8c7380afe50ef3250a27efd1
최지은 <lielieing@naver.com>
2021-03-21 16:05:01 +0900

    ch8.7 반복문 쪼개기 [리팩토링1] - 문장슬라이드

--- ch08/ch8.7 Split Loop/ch8.7 반복문쪼개기.ts
+++ ch08/ch8.7 Split Loop/ch8.7 반복문쪼개기.ts
@@ -8,14 +8,13 @@
     const p3: People = {age: 25, salary: 4000};
     const people: People[] = [p1, p2, p3];
 
-    let youngest = people[0] ? people[0].age : Infinity;
-    let totalSalary = 0;
 
-    // 관련없는 두가지 계산을 수행중
+    let youngest = people[0] ? people[0].age : Infinity;
     for (const p of people) {
         if (p.age < youngest) youngest = p.age;
     }
 
+    let totalSalary = 0;
     for (const p of people) {
         totalSalary += p.salary;
     }
